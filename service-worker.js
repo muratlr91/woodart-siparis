1b79f615bca2e5b951d5dd55efb0b2f73344d346
muratlr91 <muratlr91@gmail.com>
2026-09-18 09:38:48 +0300

    Ürün görselleri için thumbnail ve egress optimizasyonu

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='woodart-pwa-v3';
+const CACHE_NAME='woodart-pwa-v4';
 const ASSETS=['./','./index.html','./yonetim.html','./manifest.webmanifest','./woodart-icon.svg','./woodart-logo.png','./stok-urunleri.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
